70a43cac4aaf93870196611ea83c0d54001527e6
Duc Nguyen <ndpduc@gmail.com>
2017-06-09 18:07:57 +0700

    - Update project files.

--- scripts/BuildDistribution.sh
+++ scripts/BuildDistribution.sh
@@ -15,17 +15,17 @@ touch version/Version.txt
 
 # Clean and build CocosBuilder
 cd CocosBuilder/
-xcodebuild -alltargets clean
-xcodebuild -target CocosBuilder -configuration Debug build
+xcodebuild -workspace CocosBuilder.xcworkspace -scheme CocosBuilder clean
+xcodebuild -workspace CocosBuilder.xcworkspace -scheme CocosBuilder -configuration Release build
 
-# Create archives
-cd ..
-# mkdir "build/CocosBuilder-$CCB_VERSION-examples"
-mkdir "build/CocosBuilder-$CCB_VERSION-CCBReader"
-# cp -RL "Examples" "build/CocosBuilder-$CCB_VERSION-examples/"
-cp -RL "Examples/CocosBuilderExample/libs/CCBReader" "build/CocosBuilder-$CCB_VERSION-CCBReader/"
-
+## Create archives
+#cd ..
+## mkdir "build/CocosBuilder-$CCB_VERSION-examples"
+#mkdir "build/CocosBuilder-$CCB_VERSION-CCBReader"
+## cp -RL "Examples" "build/CocosBuilder-$CCB_VERSION-examples/"
+#cp -RL "Examples/CocosBuilderExample/libs/CCBReader" "build/CocosBuilder-$CCB_VERSION-CCBReader/"
+#
 cd build/
 zip -r "CocosBuilder-$CCB_VERSION.zip" CocosBuilder.app
-# zip -r "CocosBuilder-$CCB_VERSION-examples.zip" "CocosBuilder-$CCB_VERSION-examples"
-zip -r "CocosBuilder-$CCB_VERSION-CCBReader.zip" "CocosBuilder-$CCB_VERSION-CCBReader"
+## zip -r "CocosBuilder-$CCB_VERSION-examples.zip" "CocosBuilder-$CCB_VERSION-examples"
+#zip -r "CocosBuilder-$CCB_VERSION-CCBReader.zip" "CocosBuilder-$CCB_VERSION-CCBReader"
